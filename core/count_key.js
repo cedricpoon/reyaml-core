2f98574c76dd9eb3d2c04b02b1b837abf7891e82
Cedric Poon <cedric@poons.me>
2019-11-08 02:02:26 +0800

    Update count_key.js
    complete count-key

--- core/count_key.js
+++ core/count_key.js
@@ -1,7 +1,11 @@
-const count = obj => obj != null ? Object.keys(obj).reduce((acc, curr) => {
-  if (Array.isArray(obj)) return count(obj[curr])
-  else if (typeof obj[curr] === 'object') return ++acc + count(obj[curr])
-  else return ++acc
-}, 0) : 0;
+function count ({ sourceObj }) {
+  const rc = obj => obj != null ? Object.keys(obj).reduce((acc, curr) => {
+    if (Array.isArray(obj)) return acc + rc(obj[curr])
+    else if (typeof obj[curr] === 'object') return ++acc + rc(obj[curr])
+    else return ++acc
+  }, 0) : 0;
+
+  return rc(sourceObj);
+}
 
 module.exports = { count_key: count };
