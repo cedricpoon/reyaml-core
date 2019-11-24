e927540c1a71041abc24443a0e8e344a3f697ad5
Cedric Poon <cedric@poons.me>
2019-11-25 05:01:28 +0800

    Update traverse.js

--- utils/traverse.js
+++ utils/traverse.js
@@ -9,12 +9,26 @@ class Traverse {
       if (sourceObj)
         Object.keys(sourceObj).map(name => {
           if ((!Array.isArray(sourceObj) || typeof sourceObj[name] !== 'object') && index++ === lineNo) this._callback(sourceObj, name);
-          if (typeof sourceObj[name] === 'object') index = _tr({ sourceObj: sourceObj[name], lineNo, index });
+          else if (typeof sourceObj[name] === 'object') index = _tr({ sourceObj: sourceObj[name], lineNo, index });
         });
       return index;
     }
 
     _tr({ sourceObj: this._source, lineNo, index: 0 });
+
+    return this;
+  }
+
+  byObject(obj) {
+    const _tr = ({ sourceObj, obj }) => {
+      if (sourceObj)
+        Object.keys(sourceObj).map(name => {
+          if ((!Array.isArray(sourceObj) || typeof sourceObj[name] !== 'object') && obj === sourceObj[name]) this._callback(sourceObj, name);
+          else if (typeof sourceObj[name] === 'object') index = _tr({ sourceObj: sourceObj[name], obj });
+        });
+    }
+
+    _tr({ sourceObj: this._source, obj });
 
     return this;
   }
