ed3e3fcb30697eae06d10d18ad24e2b42febd09f
Cedric Poon <cedric@poons.me>
2019-11-29 19:26:05 +0800

    Progress in byLineNo()

--- utils/traverse.js
+++ utils/traverse.js
@@ -13,16 +13,29 @@ class Traverse {
 
   byLineNo(lineNo) {
     const _tr = ({ sourceObj, lineNo, index }) => {
-      if (sourceObj)
-        Object.keys(sourceObj).map(name => {
-          if ((!Array.isArray(sourceObj) || typeof sourceObj[name] !== 'object') && index++ === lineNo) this._callback(sourceObj, name);
+      if (sourceObj) {
+        const keys = Object.keys(sourceObj);
+        let i = 0;
+        for (; i < keys.length; i++) {
+          const name = keys[i];
+          if (!Array.isArray(sourceObj) || typeof sourceObj[name] !== 'object') {
+            const newLineNo = this._getNewLineNo(sourceObj[name]);
+            if (newLineNo === 0 && index === lineNo)
+              this._callback(sourceObj, name);
+            else if (newLineNo > 0) {
+              if (lineNo >= index && lineNo <= index + newLineNo)
+                this._callback(sourceObj, name);
+              index += newLineNo;
+            }
+            index++;
+            if (typeof sourceObj[name] === 'object') index = _tr({ sourceObj: sourceObj[name], lineNo, index });
+          }
           else if (typeof sourceObj[name] === 'object') index = _tr({ sourceObj: sourceObj[name], lineNo, index });
-        });
+        }
+      }
       return index;
     }
-
     _tr({ sourceObj: this._source, lineNo, index: 0 });
-
     return this;
   }
 
@@ -34,9 +47,7 @@ class Traverse {
           else if (typeof sourceObj[name] === 'object') _tr({ sourceObj: sourceObj[name], obj });
         });
     }
-
     _tr({ sourceObj: this._source, obj });
-
     return this;
   }
 
