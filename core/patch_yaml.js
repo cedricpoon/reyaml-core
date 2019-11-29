bd9e40d593394345729063dae0150c17c1197c12
Cedric Poon <cedric@poons.me>
2019-11-29 16:40:07 +0800

    Update patch_yaml.js

--- core/patch_yaml.js
+++ core/patch_yaml.js
@@ -6,11 +6,13 @@ const startWithKey = ln => getKey(ln) !== null;
 
 const endWithScalar = ln => ln.match(/:\s+[\|>][\+\-]\s*$/g) !== null;
 
-const getValue = ln => ln.match(/(?<=:\s+)[^\s]+.*$/g);
+const getValue = ln => ln.match(/(?<=:\s+)[^\s]+.*/g);
 
 const hasNoValue = ln => ln.match(/:\s*$/g) !== null;
 
 const isKeyPair = ln => startWithKey(ln) && !endWithScalar(ln) && getValue(ln) !== null;
+
+const isNode = ln => (startWithKey(ln) || isArray(ln)) && !endWithScalar(ln)
 
 const isArray = ln => ln.match(/^\s*-\s+[^-\s].*/g) !== null;
 
@@ -22,45 +24,56 @@ const replace = (ln, map) => Object
   .keys(map)
   .reduce((retn, k) => retn.replace(new RegExp(k, 'g'), map[k]), ln);
 
+const _trLn = (yStr, f) => yStr
+  .split('\n')
+  .reduce(({ result, prevIndent }, ln, i, yamlArr) =>
+    i === 0 || isNode(yamlArr[i - 1]) || countIndent(ln) < prevIndent ?
+      {
+        result: result + `${f(
+          (i - 1 > 0) ? yamlArr[i - 1] : null,
+          ln,
+          (i + 1 < yamlArr.length) ? yamlArr[i + 1] : null
+        )}\n`,
+        prevIndent: countIndentWithHyphen(ln)
+      }
+    :
+      { result: result + `${ln}\n`, prevIndent }
+  , { result: '', prevIndent: 0 });
+
+const traverseLine = (yamlString, callback) => {
+  const { result, prevIndent } = _trLn(yamlString, callback);
+  return result;
+}
+
 function wrapKeyPair(yamlString) {
-  return yamlString
-    .split('\n')
-    .reduce((a, ln) =>
-      isKeyPair(ln)
-      ?
-        a += getKey(ln) + ` ${literalBlockChoppingScalar}\n` + ' '.repeat(countIndentWithHyphen(ln) + 2) + getValue(ln) + '\n'
-      :
-        a += ln + '\n'
-      , '')
+  return traverseLine(yamlString, (prev, curr, next) => {
+    if (isKeyPair(curr))
+      return getKey(curr) + ` ${literalBlockChoppingScalar}\n` + ' '.repeat(countIndentWithHyphen(curr) + 2) + getValue(curr);
+    else
+      return curr;
+  });
 }
 
 function appendBlockScalar(yamlString) {
-  const yamlArr = yamlString.split('\n');
-  return yamlArr
-    .map((x, i) => {
-      if (i + 1 < yamlArr.length && startWithKey(x) && hasNoValue(x) && !startWithKey(yamlArr[i + 1]) && !isArray(yamlArr[i + 1]))
-        return `${x} ${literalBlockScalar}`;
-      return x;
-    })
-    .join('\n');
+  return traverseLine(yamlString, (prev, curr, next) => {
+    if (next !== null && startWithKey(curr) && hasNoValue(curr) && !startWithKey(next) && !isArray(next))
+      return `${curr} ${literalBlockScalar}`;
+    else
+      return curr
+  });
 }
 
 function unifyBlockScalar(yamlString) {
-  let prevIndent = 0;
-  return yamlString
-    .split('\n')
-    .map(x => {
-      if (startWithKey(x) || isArray(x)) {
-        if (countIndent(x) <= prevIndent) x = replace(x, blockScalar4Traverse);
-        prevIndent = countIndentWithHyphen(x);
-      }
-      return x;
-    })
-    .join('\n');
+  return traverseLine(yamlString, (prev, curr, next) => {
+    if (startWithKey(curr) || isArray(curr))
+      return replace(curr, blockScalar4Traverse);
+    return curr;
+  });
 }
 
 function patch({ yamlString }) {
-  const result = appendBlockScalar(wrapKeyPair(unifyBlockScalar(yamlString)));
+  let result = appendBlockScalar(wrapKeyPair(unifyBlockScalar(yamlString)));
+  result = result.replace(/\n*$/g, '');
   return result;
 }
 
