97289cd8cc24c9083aa7fda2b1f9782e17dd0bda
Cedric Poon <cedric@poons.me>
2019-11-30 03:48:33 +0800

    :)

--- config.js
+++ config.js
@@ -1,44 +1,44 @@
-const rootName = 'root';
-
-const marker = { name: '*', content: '**' }
-
 const literalBlockScalar = '|+';
 
-const literalBlockChoppingScalar = '|-';
-
-const blockScalar4Traverse = { '\\|\\-': literalBlockScalar, '>\\-': literalBlockScalar, '>\\+': literalBlockScalar };
-
-const markerMap = {
-  highlight: {
-    name: 'highlight',
-    d3: {
-      nodeSvgShape: {
-        shape: 'circle',
-        shapeProps: {
-          r: 10,
-          fill: 'red',
-          stroke: 'red',
+const config = {
+  rootName: 'root',
+  literalBlockScalar: literalBlockScalar,
+  literalBlockChoppingScalar: '|-',
+  blockScalar4Traverse: { '\\|\\-': literalBlockScalar, '>\\-': literalBlockScalar, '>\\+': literalBlockScalar },
+  maxStringLength: 17,
+  marker: { name: '*', content: '**' },
+  markerMap: {
+    highlight: {
+      name: 'highlight',
+      d3: {
+        nodeSvgShape: {
+          shape: 'circle',
+          shapeProps: {
+            r: 10,
+            fill: 'red',
+            stroke: 'red',
+          },
         },
-      },
-    }
-  },
-  truncated: {
-    name: 'truncated',
-    d3: {
-      attributes: { '': '.....' },
-      nodeSvgShape: {
-        shape: 'rect',
-        shapeProps: {
-          width: 20,
-          height: 20,
-          x: -10,
-          y: -10,
-          stroke: 'lightgrey',
-          fill: 'lightgrey',
-        }
-      },
+      }
+    },
+    truncated: {
+      name: 'truncated',
+      d3: {
+        attributes: { '': '.....' },
+        nodeSvgShape: {
+          shape: 'rect',
+          shapeProps: {
+            width: 20,
+            height: 20,
+            x: -10,
+            y: -10,
+            stroke: 'lightgrey',
+            fill: 'lightgrey',
+          }
+        },
+      }
     }
   }
-}
+};
 
-module.exports = { rootName, marker, markerMap, blockScalar4Traverse, literalBlockScalar, literalBlockChoppingScalar };
+module.exports = config;
