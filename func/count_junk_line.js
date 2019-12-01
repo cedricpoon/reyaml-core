5371f36089c9e087de63d20f17f7302ea1ce7a75
Cedric Poon <cedric@poons.me>
2019-12-02 03:53:46 +0800

    complete patch_yaml()

--- func/count_junk_line.js
+++ func/count_junk_line.js
@@ -6,6 +6,8 @@ const isWhiteline = ln => ln.match(/^\s*$/g) !== null;
 
 const isJunk = ln => isWhiteline(ln) | isComment(ln) | isArraySeparator(ln);
 
+const isParserIgnorable = ln => isWhiteline(ln) | isComment(ln);
+
 const count = ({ sourceYaml, lineNo }) => {
   const arr = sourceYaml.split('\n');
   return arr.reduce((acc, curr, i) => acc += (isJunk(curr) && i <= lineNo) ? 1 : 0, 0);
@@ -13,5 +15,6 @@ const count = ({ sourceYaml, lineNo }) => {
 
 module.exports = {
   count_junk_line: count,
-  is_junk_line: isJunk
+  is_junk_line: isJunk,
+  is_parser_ignorable: isParserIgnorable
 };
